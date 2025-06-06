8dc20c9dfa5bdec9c8c296c49bfe6e8ab5b74979
Osioreame Ikiebe <reameikiebe@gmail.com>
2025-06-06 16:23:14 -0400

    Modified components to use React Router for navigation, added Layouts, Error page, fixed nav bar button hovering and added json-server for mock api

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,19 @@ import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
 import ViewAllJobs from './components/ViewAllJobs'
+import MainLayout from './layouts/MainLayout'
+import HomePage from './pages/HomePage'
+import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage />} />
+    <Route path='/jobs' element={<JobsPage />} />
+    <Route path='*' element={<NotFoundPage />} />
+  </Route>
+  )
 );
 
 

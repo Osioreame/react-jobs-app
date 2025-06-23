11b32bf861afccf7542ce92e165bddbd06594930
Osioreame Ikiebe <reameikiebe@gmail.com>
2025-06-23 18:53:18 -0400

    Added data loader, single job page AddJob page and functionality to add new jobs to the jobs page

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,34 @@ import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
 import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
-import JobPage from './Pages/JobPage'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePage />} />
-    <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/jobs/:id' element={<JobPage />} />
-    <Route path='*' element={<NotFoundPage />} />
-  </Route>
-  )
-);
-
+import JobPage, {jobLoader} from './Pages/JobPage'
+import AddJobPage from './pages/AddJobPage'
 
 const App = () => {
+  // Add a new job to the database via API
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(newJob),
+  });
+  return;
+}
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} /> 
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+    )
+  );
+  
   return <RouterProvider router = {router}/>
 }
 

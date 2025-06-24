03790ab433816aefee524cae604b62900a5048a0
Osioreame Ikiebe <reameikiebe@gmail.com>
2025-06-23 20:14:19 -0400

    Added delete and edit functionality, added toast package

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,10 @@ import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage, {jobLoader} from './Pages/JobPage'
 import AddJobPage from './pages/AddJobPage'
+import EditJobPage from './pages/EditJobPage'
 
 const App = () => {
-  // Add a new job to the database via API
+  // Add new job via API
   const addJob = async (newJob) => {
     const res = await fetch('/api/jobs', {
       method: 'POST',
@@ -24,14 +25,35 @@ const App = () => {
   });
   return;
 }
+
+//Delete job
+const deleteJob = async (id) => {
+  const res = await fetch(`/api/jobs/${id}`, {
+    method: 'DELETE',
+  });
+return;
+};
   
+//Update job
+const updateJob = async (job) => {
+  const res = await fetch(`/api/jobs/${job.id}`, {
+    method: 'PUT',
+    headers: {
+    'Content-type': 'application/json',
+  },
+  body: JSON.stringify(job),
+});
+return;
+}
+
   const router = createBrowserRouter(
     createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
       <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} /> 
+      <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader} /> 
+      <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob}/>} loader={jobLoader} /> 
       <Route path='*' element={<NotFoundPage />} />
     </Route>
     )
